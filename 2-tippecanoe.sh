8f3f91f70b95b877f87b9d86a2e239192dff2107
Christopher Lee <sirleech@gmail.com>
2026-07-17 21:54:18 +1000

    fix

--- 2-tippecanoe.sh
+++ 2-tippecanoe.sh
@@ -1,6 +1,1 @@
-tippecanoe -o contours.pmtiles \
-  -zg \
-  --drop-densest-as-needed \
-  -l contours \    
-  contours.geojsonl
-
+tippecanoe -o contours.pmtiles  -zg --drop-densest-as-needed -l contours contours.geojsonl
